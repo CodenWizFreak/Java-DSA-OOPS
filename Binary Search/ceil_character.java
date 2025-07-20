6f42d7654b7dd5bbe141a1d5a41272bebecd0558
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-07-20 12:16:53 +0530

    Update ceil_character.java

--- Binary Search/ceil_character.java
+++ Binary Search/ceil_character.java
@@ -22,4 +22,21 @@ class Solution {
     }
 }
 
-// Time complexity is same for both but by space complexity, more Optimized Solution is the below one. 
+// Time complexity is same for both but by space complexity, more Optimized Solution is the below one (not much optimized). 
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0, end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (letters[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start%letters.length];
+    }
+}
