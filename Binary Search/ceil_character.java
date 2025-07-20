07d663006d51c531d00589420bdc0d6693e8c361
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-07-20 11:46:16 +0530

    Create ceil_character.java

---
+++ Binary Search/ceil_character.java
@@ -0,0 +1,25 @@
+// Leetcode Problem 744
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char minceil = 'z';
+        int start = 0, end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (letters[mid] > target) {
+                if (letters[mid] < minceil)
+                    minceil = letters[mid];
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        if(target==letters[letters.length-1]||target=='z'|| new String(letters).indexOf(minceil) == -1)
+            return letters[0];
+        return minceil;
+    }
+}
+
+// Time complexity is same for both but by space complexity, more Optimized Solution is the below one. 
