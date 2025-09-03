a6a453c5d94fc715cb586b3ff4f745100312396f
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-09-03 07:31:37 +0530

    Create FindAllMissing.java

---
+++ Cyclic Sort/FindAllMissing.java
@@ -0,0 +1,59 @@
+// Leetcode Problem 448
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FindAllMissing {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        // just find missing numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index+1) {
+                ans.add(index + 1);
+            }
+        }
+
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
+
+//OR
+
+/*
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+    }
+}
+
+*/
