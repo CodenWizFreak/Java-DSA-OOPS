adf7a5d4f0afeda4b58b50efc447b5d7d418bc0d
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2026-01-21 08:15:20 +0530

    Implement palindrome check for linked list

---
+++ Linked List/palindromelist.java
@@ -0,0 +1,42 @@
+// Leetcode problem 234
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) 
+          return true;
+
+        // 1. Find middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 2. Reverse second half
+        ListNode secondHalf = reverse(slow);
+
+        // 3. Compare both halves
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) 
+              return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
