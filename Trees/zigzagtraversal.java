4e634906268a867c78d70ea2056caeb9358c071d
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2026-02-27 08:46:15 +0530

    Create zigzagtraversal.java

---
+++ Trees/zigzagtraversal.java
@@ -0,0 +1,49 @@
+// Leetcode problem 103
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        dq.offer(root);
+
+        boolean leftToRight = true;
+
+        while (!dq.isEmpty()) {
+            int levelSize = dq.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) 
+            {
+                if (leftToRight) 
+                {
+                    TreeNode node = dq.pollFirst();
+                    currentLevel.add(node.val);
+
+                    if (node.left != null) 
+                        dq.offerLast(node.left);
+                    if (node.right != null) 
+                        dq.offerLast(node.right);
+                } 
+                else 
+                {
+                    TreeNode node = dq.pollLast();
+                    currentLevel.add(node.val);
+
+                    // Important: reverse order
+                    if (node.right != null) 
+                        dq.offerFirst(node.right);
+                    if (node.left != null) 
+                        dq.offerFirst(node.left);
+                }
+            }
+
+            result.add(currentLevel);
+            leftToRight = !leftToRight; // toggle direction
+        }
+
+        return result;
+    }
+}
