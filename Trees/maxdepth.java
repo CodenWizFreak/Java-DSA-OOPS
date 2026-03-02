e5838c6882d34a0c5aa6b8c0591c26ba0ac5d5bd
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2026-03-02 14:06:13 +0530

    Add maxDepth method for binary tree depth calculation
    Implement maxDepth method to calculate the depth of a binary tree.

---
+++ Trees/maxdepth.java
@@ -0,0 +1,25 @@
+// leetcode problem 104
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        int max = 1+(int)Math.max(maxDepth(root.left),maxDepth(root.right));   
+        return max;
+    }
+}
